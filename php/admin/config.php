66d5582b94a8a35c23fce7f0717ef9b11e6e0071
DESKTOP-DD5CSQD\Ruslan <ramag>
2019-04-13 15:49:32 +0300

    init

---
+++ php/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://oc-test.loc/admin/');
+define('HTTP_CATALOG', 'http://oc-test.loc/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://oc-test.loc/admin/');
+define('HTTPS_CATALOG', 'http://oc-test.loc/');
+
+// DIR
+define('DIR_APPLICATION', '/app/admin/');
+define('DIR_SYSTEM', '/app/system/');
+define('DIR_IMAGE', '/app/image/');
+define('DIR_LANGUAGE', '/app/admin/language/');
+define('DIR_TEMPLATE', '/app/admin/view/template/');
+define('DIR_CONFIG', '/app/system/config/');
+define('DIR_CACHE', '/app/system/storage/cache/');
+define('DIR_DOWNLOAD', '/app/system/storage/download/');
+define('DIR_LOGS', '/app/system/storage/logs/');
+define('DIR_MODIFICATION', '/app/system/storage/modification/');
+define('DIR_UPLOAD', '/app/system/storage/upload/');
+define('DIR_CATALOG', '/app/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'db');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'demo1234');
+define('DB_DATABASE', 'teststore');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
